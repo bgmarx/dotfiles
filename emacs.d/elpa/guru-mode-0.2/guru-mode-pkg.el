28ab3b0d45053855469ee4bd86c903b642de1dab
Ben Marx <benyamin.marx@gmail.com>
2014-11-04 18:04:45 +0900

    new emacs

---
+++ emacs.d/elpa/guru-mode-0.2/guru-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "guru-mode" "0.2" "Become an Emacs guru" 'nil :url "https://github.com/bbatsov/guru-mode" :keywords '("convenience"))
---
+++ emacs.d/init.el
@@ -0,0 +1,47 @@
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(setq inhibit-startup-message t)
+
+(require 'cl)
+(require 'package)
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(unless (require 'el-get nil t)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://github.com/dimitri/el-get/raw/master/el-get-install.el")
+    (end-of-buffer)
+    (eval-print-last-sexp)))
+
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("org" . "http://orgmode.org/elpa/")
+                        ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("melpa-stable" . "http://melpa-stable.milkbox.net/packages/")))
+(package-initialize)
+
+(defun require-package (package)
+  (setq-default highlight-tabs t)
+  (unless (package-installed-p package)
+    (unless (assoc package package-archive-contents)
+      (package-refresh-contents))
+    (package-install package)))
+(display-time-mode t)
+
+(if (member "Monaco" (font-family-list))
+    (set-face-attribute
+          'default nil :font "Monaco 12"))
+
+(global-linum-mode t)
+(setq linum-format "%d ")
+(setq-default tab-width 4 indent-tabs-mode nil)
+(define-key global-map (kbd "RET") 'newline-and-indent)
+(scroll-bar-mode -1)
+(setq gdb-many-windows t)
+(guru-global-mode +1)
+(setq guru-warn-only t)
+(require 'helm-config)
+(global-set-key (kbd "C-c h") 'helm-mini)
+(helm-mode 1)
+(powerline-default-theme)
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
